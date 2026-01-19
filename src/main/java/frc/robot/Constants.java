a8582006c5b4825761ac1d7e1f2ba8b6ec400542
Willie Pai <PaisWillie@gmail.com>
2026-01-19 16:41:30 -0500

    fix: linting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,16 +7,15 @@ public final class Constants {
     public static final int DRIVER_CONTROLLER_PORT = 0;
 
     public static final class SwerveConstants {
-        public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+        public static final double MAX_SPEED = Units.feetToMeters(14.5);
     }
 
-    public static class OperatorConstants
-  {
+    public static class OperatorConstants {
 
-    // Joystick Deadband
-    public static final double DEADBAND        = 0.1;
-    public static final double LEFT_Y_DEADBAND = 0.1;
-    public static final double RIGHT_X_DEADBAND = 0.1;
-    public static final double TURN_CONSTANT    = 6;
-  }
+        // Joystick Deadband
+        public static final double DEADBAND = 0.1;
+        public static final double LEFT_Y_DEADBAND = 0.1;
+        public static final double RIGHT_X_DEADBAND = 0.1;
+        public static final double TURN_CONSTANT = 6;
+    }
 }
