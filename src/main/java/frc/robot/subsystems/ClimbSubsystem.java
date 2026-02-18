6add6f6cf6d7410e27c9fe26db77aa76192be060
Willie Pai <PaisWillie@gmail.com>
2026-02-18 12:02:47 -0800

    feat: added climber cmds

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.climb.ElevatorSubsystem;
 import frc.robot.subsystems.climb.TongueSubsystem;
@@ -13,9 +15,63 @@ public class ClimbSubsystem extends SubsystemBase {
     private final ElevatorSubsystem m_elevatorSubsystem;
     private final TongueSubsystem m_tongueSubsystem;
 
+    private enum ClimbPosition {
+        NONE, L1, L2, L3
+    }
+
+    private ClimbPosition m_currentClimbPosition;
+
     public ClimbSubsystem() {
         m_elevatorSubsystem = new ElevatorSubsystem();
         m_tongueSubsystem = new TongueSubsystem();
+        m_currentClimbPosition = ClimbPosition.NONE;
+    }
+
+    private void setClimbPosition(ClimbPosition newPosition) {
+        m_currentClimbPosition = newPosition;
+    }
+
+    // TODO
+    public Command climbToL1() {
+        return Commands.sequence(Commands.none()) // TODO
+                .withName("CLMB - Climb to L1 from ground")
+                .onlyIf(() -> m_currentClimbPosition == ClimbPosition.NONE)
+                .andThen(Commands.runOnce(() -> setClimbPosition(ClimbPosition.L1)));
+    }
+
+    // TODO
+    public Command climbToL2() {
+
+        Command L1toL2 = Commands.sequence(Commands.none()) // TODO
+                .withName("CLMB - Climb to L2 from L1")
+                .andThen(Commands.runOnce(() -> setClimbPosition(ClimbPosition.L2)));
+
+        switch (m_currentClimbPosition) {
+            case NONE:
+                return Commands.sequence(climbToL1(), L1toL2);
+            case L1:
+                return L1toL2;
+            default:
+                return Commands.none();
+        }
+    }
+
+    public Command climbToL3() {
+
+        Command L2toL3 = Commands.sequence(Commands.none()) // TODO
+                .withName("CLMB - Climb to L3 from L2")
+                .andThen(Commands.runOnce(() -> setClimbPosition(ClimbPosition.L3)));
+
+        switch (m_currentClimbPosition) {
+            case NONE:
+                return Commands.sequence(climbToL1(), climbToL2(), L2toL3);
+            case L1:
+                return Commands.sequence(climbToL2(), L2toL3);
+            case L2:
+                return L2toL3;
+            default:
+                return Commands.none();
+        }
     }
 
     @Override
