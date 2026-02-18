68900d0a100073a51ac837d475a05ca4bbc0b7e0
Willie Pai <PaisWillie@gmail.com>
2026-02-17 20:03:55 -0800

    fix: remove starting position since we have absolute encoder

--- src/main/java/frc/robot/subsystems/shooter/HoodSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/HoodSubsystem.java
@@ -86,9 +86,7 @@ public class HoodSubsystem extends SubsystemBase {
                 .withSoftLimits(HoodConstants.SOFT_LIMIT_MIN, HoodConstants.SOFT_LIMIT_MAX)
                 .withHardLimit(HoodConstants.HARD_LIMIT_MIN, HoodConstants.HARD_LIMIT_MAX)
                 .withLength(HoodConstants.LENGTH)
-                .withMass(HoodConstants.MASS)
-                .withStartingPosition(HoodConstants.STARTING_ANGLE); // TODO: Might not need since we have absolute
-                                                                     // encoder
+                .withMass(HoodConstants.MASS);
 
         m_hood = new Arm(m_hoodConfig);
     }
