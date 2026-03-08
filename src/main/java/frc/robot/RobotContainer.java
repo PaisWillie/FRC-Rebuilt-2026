6d99b8e1e2bc5d90c01ab51b33dd10dadca55694
Willie Pai <PaisWillie@gmail.com>
2026-03-07 19:54:14 -0500

    feat: re-add drive code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,18 +27,41 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import frc.robot.Constants.ClimbConstants.ElevatorConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.SwerveSubsystem;
 import frc.robot.subsystems.climb.ElevatorSubsystem;
+import swervelib.SwerveInputStream;
 
 public class RobotContainer {
     final CommandPS5Controller m_driverController = new CommandPS5Controller(Constants.DRIVER_CONTROLLER_PORT);
 
     private final ElevatorSubsystem m_elevatorSubsystem = new ElevatorSubsystem();
+    private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
+            "swerve"));
+
+    /**
+     * Converts driver input into a field-relative ChassisSpeeds that is controlled
+     * by angular velocity.
+     */
+    SwerveInputStream driveAngularVelocity = SwerveInputStream.of(m_swerveSubsystem.getSwerveDrive(),
+            () -> m_driverController.getLeftY() * -1,
+            () -> m_driverController.getLeftX() * -1)
+            .withControllerRotationAxis(() -> m_driverController.getRightX() * -1) // TODO: Check if * -1 is
+                                                                                   // needed IRL
+            .deadband(OperatorConstants.DEADBAND)
+            .scaleTranslation(1.0)
+            .allianceRelativeControl(true);
 
     public RobotContainer() {
         configureBindings();
     }
 
     private void configureBindings() {
+        Command driveFieldOrientedAngularVelocity = m_swerveSubsystem.driveFieldOriented(driveAngularVelocity);
+
+        m_swerveSubsystem.setDefaultCommand(driveFieldOrientedAngularVelocity);
+        m_driverController.options().onTrue((Commands.runOnce(m_swerveSubsystem::zeroGyro)));
+
         m_driverController.cross().whileTrue(m_elevatorSubsystem.sysId());
         m_driverController.povDown().onTrue(m_elevatorSubsystem.setHeight(ElevatorConstants.STARTING_HEIGHT));
         m_driverController.povLeft().onTrue(m_elevatorSubsystem.setHeight(ElevatorConstants.FIRST_LEVEL_HEIGHT));
